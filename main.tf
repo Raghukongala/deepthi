8cd85f5339242f8bc8634c7f9cc0830479eb576f
Raghukongala <kongalaraghu406@gmail.com>
2026-04-16 19:19:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -278,23 +278,24 @@ resource "aws_appautoscaling_target" "ecs" {
 # VPC ENDPOINTS (ECR + Logs)
 ############################
 resource "aws_vpc_endpoint" "ecr_api" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.ap-south-1.ecr.api"
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = aws_subnet.private[*].id
+  vpc_id             = aws_vpc.main.id
+  service_name       = "com.amazonaws.ap-south-1.ecr.api"
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = aws_subnet.private[*].id
   security_group_ids = [aws_security_group.ecs_sg.id]
 }
 
 resource "aws_vpc_endpoint" "ecr_dkr" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.ap-south-1.ecr.dkr"
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = aws_subnet.private[*].id
+  vpc_id             = aws_vpc.main.id
+  service_name       = "com.amazonaws.ap-south-1.ecr.dkr"
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = aws_subnet.private[*].id
   security_group_ids = [aws_security_group.ecs_sg.id]
 }
 
 resource "aws_vpc_endpoint" "logs" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.ap-south-1.logs"
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = aws_subnet.private[*].id
+  vpc_id             = aws_vpc.main.id
+  service_name       = "com.amazonaws.ap-south-1.logs"
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = aws_subnet.private[*].id
+  security_group_ids
